0816707e11f98ebdb3b466a5b8be502fdab4acfc
HarrisAva <avaharris1441@gmail.com>
2024-02-22 10:44:50 -0600

    complete authentication and user model/controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+    def authenticate_request
+        header = request.headers['Authorization']
+        header = header.split{' '}.last if header
+        begin
+            decoded = JWT.decode(header, Rails.application.secrets.secret_key_base).first
+            @current_user = User.find(decoded['user.id'])
+        rescue JWT::ExpiredSignature
+            render json: {error: 'Token has expired'}, status: :unauthorized
+        rescue JWT::DecodeError
+            render json: {errors: 'Unathorized'}, status: :unauthorized
+        
+        end
+    end
 end
